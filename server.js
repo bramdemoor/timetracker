49732bc45bc42af8055d578ac6505d2b0608115e
Bram De Moor <bram.demoor@gmail.com>
2013-03-23 18:55:11 +0100

    Server-side cleanup + removing reports

--- server.js
+++ server.js
@@ -6,3 +6,27 @@ app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
+
+/*
+	getAllEntries: "SELECT * FROM entries" (getAllEntries order by Start asc)
+		
+	createNewEntry => insertTask($tsCode, $description, $start)
+	createNewEntry => createNewEntry($vars['tsCode'], $vars['description'], time());
+	// TODO: Do insert using these values
+	
+	updateTaskDescription => updateTaskDescription($id, $task)
+	updateTaskDescription => updateTaskDescription($vars['value'], $vars['pk']);
+	// TODO: Update 'Task' using Id
+	
+	removeEntry => deleteEntry($id)
+	removeEntry => removeEntry($vars['pk']);
+	// TODO: Simple delete by Id
+	
+	getEntryNames => getDistinctItems	
+	getEntryNames => getEntryNames (json!)
+	"SELECT DISTINCT Task FROM entries"
+	
+	getTsCodes => getDistinctTsCodes
+	getTsCodes => getTsCodes (json!)
+	"SELECT DISTINCT TSCode FROM entries"
+*/
