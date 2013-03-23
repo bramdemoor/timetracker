29a28b39ef7f74f6b566e212222aa119d4e137c9
Bram De Moor <bram.demoor@gmail.com>
2013-03-23 19:15:40 +0100

    api update

--- server.js
+++ server.js
@@ -5,32 +5,24 @@ var app = express();
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/entry', function(req, res){
+app.get('/entries', function(req, res){
   res.json([{Id:1,TSCode:'ABC123',Task:'My first entry',Start:new Date()}]);
+});
+
+app.get('/entrynames', function(req, res){
+  res.json(['My first entry']);
+});
+
+app.get('/tsccodes', function(req, res){
+  res.json(['ABC123']);
 });
 
 app.listen(8080);
 
 /*
-	getAllEntries: "SELECT * FROM entries" (getAllEntries order by Start asc)
-		
-	createNewEntry => insertTask($tsCode, $description, $start)
+	TODO: Implement CRUD
+	
 	createNewEntry => createNewEntry($vars['tsCode'], $vars['description'], time());
-	// TODO: Do insert using these values
-	
-	updateTaskDescription => updateTaskDescription($id, $task)
 	updateTaskDescription => updateTaskDescription($vars['value'], $vars['pk']);
-	// TODO: Update 'Task' using Id
-	
-	removeEntry => deleteEntry($id)
 	removeEntry => removeEntry($vars['pk']);
-	// TODO: Simple delete by Id
-	
-	getEntryNames => getDistinctItems	
-	getEntryNames => getEntryNames (json!)
-	"SELECT DISTINCT Task FROM entries"
-	
-	getTsCodes => getDistinctTsCodes
-	getTsCodes => getTsCodes (json!)
-	"SELECT DISTINCT TSCode FROM entries" 
 */
